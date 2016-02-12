a74e2a698ef4bb5768e7978cd11e190310d71279
Fabian Trampusch <fabian.trampusch@freenet.de>
2016-02-12 14:13:09 +0100

    Adds categories and notes relationship

--- notes-frontend/src/main/resources/frontend/scripts/categories.ts
+++ notes-frontend/src/main/resources/frontend/scripts/categories.ts
@@ -2,6 +2,7 @@ module Notes {
 
     export class Category {
         public title: String = "";
+        public _links: any;
     }
 
     class CategoriesController {
@@ -9,21 +10,28 @@ module Notes {
         public newCategoryName = "";
         public isCreatingNewCategory = false;
         public selectedCategory: Category;
+        private $http: ng.IHttpService;
+        private $rootScope;
 
-        public constructor() {
-            let category = new Category();
-            category.title = "Standardkategorie";
-            this.categories.push(category);
-            this.selectedCategory = this.categories[0];
+        public constructor($http: ng.IHttpService, $rootScope: ng.IScope) {
+            this.$rootScope = $rootScope;
+            this.$http = $http;
+            $http.get("/api/categories").then((response: ng.IHttpPromiseCallbackArg<any>) => {
+                this.categories = response.data._embedded.categories;
+                this.select(this.categories[0]);
+            });
         }
 
         public select(category: Category) {
             this.selectedCategory = category;
+            this.$rootScope.$broadcast("categories.selected", category);
         }
 
         public delete(category: Category) {
-            let index = this.categories.indexOf(category);
-            this.categories.splice(index, 1);
+            this.$http.delete(category._links.self.href).then(() => {
+                let index = this.categories.indexOf(category);
+                this.categories.splice(index, 1);
+            });
         }
 
         public cancel() {
@@ -38,8 +46,11 @@ module Notes {
         public saveNewCategory() {
             let category = new Category();
             category.title = this.newCategoryName;
-            this.categories.push(category);
-            this.cancel();
+            this.$http.post("/api/categories", category)
+                    .then((response: ng.IHttpPromiseCallbackArg<any>) => {
+                this.categories.push(response.data);
+                this.cancel();
+            });
         }
 
     }
--- notes-frontend/src/main/resources/frontend/scripts/notes.ts
+++ notes-frontend/src/main/resources/frontend/scripts/notes.ts
@@ -5,6 +5,7 @@ module Notes {
     class Note {
         public title: String;
         public content: String;
+        public category: String;
         public _links: any;
     }
 
@@ -15,18 +16,34 @@ module Notes {
         public notes: Note[] = [];
         public $http: ng.IHttpService;
 
-        public constructor($http: ng.IHttpService, $interval: ng.IIntervalService) {
+        public constructor($http: ng.IHttpService, $interval: ng.IIntervalService,
+                            $rootScope: ng.IScope) {
+
+            $rootScope.$on("categories.selected", (event, category: Category) => {
+                this.selectedCategory = category;
+                this.reloadNotes();
+            })
             this.$http = $http;
-            $http.get("/api/notes").then((response: ng.IHttpPromiseCallbackArg<any>) => {
-                this.notes = response.data._embedded.notes;
-                this.selectedNote = this.notes[0];
-            });
 
             $interval(() => this.save(), 1000);
         }
 
+        public reloadNotes() {
+            var config = {
+                params: {
+                    category: this.selectedCategory._links.self.href
+                }
+            };
+            this.$http.get("/api/notes/search/findByCategory", config)
+                    .then((response: ng.IHttpPromiseCallbackArg<any>) => {
+                this.notes = response.data._embedded.notes;
+                this.selectedNote = this.notes[0];
+            });
+        }
+
         public create() {
             let note = new Note();
+            note.category = this.selectedCategory._links.self.href;
             this.$http.post("/api/notes", note).then((response: ng.IHttpPromiseCallbackArg<any>) => {
                 console.log(response.data);
                 this.notes.push(response.data);
@@ -38,12 +55,17 @@ module Notes {
         }
 
         public delete(note: Note) {
-            let index = this.notes.indexOf(note);
-            this.notes.splice(index, 1);
+            this.$http.delete(note._links.self.href).then(() => {
+                let index = this.notes.indexOf(note);
+                this.notes.splice(index, 1);
+                this.selectedNote = this.notes[0];
+            });
         }
 
         public save() {
-            this.$http.put(this.selectedNote._links.self.href, this.selectedNote);
+            if (this.selectedNote) {
+                this.$http.put(this.selectedNote._links.self.href, this.selectedNote);
+            }
         }
 
     }
